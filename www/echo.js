2799343d54d66458ec181db2700ccf73f420807b
unknown <hqbn83@HQBN83-02.ds.mot.com>
2015-05-29 17:49:44 +0530

    added android source with changes

--- www/echo.js
+++ www/echo.js
@@ -1,5 +1,8 @@
+var echo = {
 window.echo = function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
         }, "Echo", "echo", [str]);
-    };
+    }
+}
+module.exports = echo;
