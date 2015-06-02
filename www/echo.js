f470cc684260cd13f0b0995cc404b1f1ae87ae7e
unknown <hqbn83@HQBN83-02.ds.mot.com>
2015-06-02 17:09:30 +0530

    changed the syntx of javascipt interfcae

--- www/echo.js
+++ www/echo.js
@@ -1,8 +1,8 @@
-var echo = {
-window.echo = function(str, callback) {
+var EchoPlugin = {
+	echo: function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
         }, "Echo", "echo", [str]);
     }
 }
-module.exports = echo;
+module.exports = EchoPlugin;
